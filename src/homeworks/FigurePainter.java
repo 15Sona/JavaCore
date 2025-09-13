4dee94b5ef8ef35373eea454045f4da5c547e770
15Sona <sona95654@gmail.com>
2025-09-13 12:52:20 +0400

    homework 2 nested for loop change a 5 as a parameter

--- src/homeworks/FigurePainter.java
+++ src/homeworks/FigurePainter.java
@@ -24,8 +24,8 @@ public class FigurePainter {
         System.out.println();
 
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i; j++) {
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
