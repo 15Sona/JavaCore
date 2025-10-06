0a2ebb082386b6a4816623e8c6e8f9d4b7f1a318
15Sona <sona95654@gmail.com>
2025-10-06 22:14:19 +0400

    array's

--- src/homeworks/dynamicarray/DynamicArray.java
+++ src/homeworks/dynamicarray/DynamicArray.java
@@ -13,7 +13,7 @@ public class DynamicArray {
         size++;
     }
 
-    public void extend() {
+    private void extend() {
         int[] newArray = new int[array.length + 10];
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
@@ -35,5 +35,65 @@ public class DynamicArray {
 
         }
         System.out.println();
+    }
+
+    //ջնջել մասիվի index-երորդ էլեմենտը
+    //եթե չկա նման ինդեքսով էլեմենտ, տպել, որ չկա նման էլեմենտ
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > array.length - 1) {
+            System.out.println("There is not such an element. Invalid index");
+            return;
+        }
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+    }
+
+    //տրված value-ն դնելու ենք տրված index-ով էլեմենտի տեղը։
+    //Հին արժեքը կկորի
+    //եթե նման ինդեքսով էլեմենտ չկա, գրում ենք որ չկա։
+    public void set(int index, int value) {
+        if (index < 0 || index > array.length) {
+            System.out.println("There is not such an element. Invalid index");
+            return;
+        } else {
+            array[index] = value;
+        }
+
+
+    }
+
+    //ավելացնել տրված value-ն տրված ինդեքսում, իսկ էղած էլեմենտները մի հատ աջ տանել։
+    //Եթե չկա նման ինդեքս, տպել որ չկա
+    public void add(int index, int value) {
+        if (index < 0 || index >= array.length) {
+            System.out.println("There is not such an element. Invalid index");
+            return;
+        }
+        for (int i = array.length-1; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+    }
+
+     //Վերադարձնել true եթե տրված value-ն կա մեր մասիվի մեջ, եթե ոչ false
+     public boolean exists(int value) {
+         for (int i = 0; i < array.length; i++) {
+             if (array[i] == value) {
+                 return true;
+             }
+         }
+         return false;
+     }
+
+    //Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+    //եթե չկա, -1
+    public int getIndexByValue ( int value){
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == value){
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/homeworks/dynamicarray/DynamicArrayDemo.java
+++ src/homeworks/dynamicarray/DynamicArrayDemo.java
@@ -3,17 +3,20 @@ package homeworks.dynamicarray;
 public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray dynamicArray = new DynamicArray();
-        for (int i = 5; i < 20 ; i+=2) {
+        for (int i = 0; i < 8 ; i++) {
             dynamicArray.add(i);
         }
         dynamicArray.print();
         System.out.println(" " + dynamicArray.getByIndex(2));
         System.out.println(" " + dynamicArray.getByIndex(10));
+        dynamicArray.deleteByIndex(6);
+        dynamicArray.set(2, 5);
+        dynamicArray.add(3, 89);
+        System.out.println(dynamicArray.exists(4));
+        System.out.println(dynamicArray.exists(90));
+        System.out.println(dynamicArray.getIndexByValue(5));
+        System.out.println(dynamicArray.getIndexByValue(90));
+
+        dynamicArray.print();
     }
 }
-
-
-
-
-
-
